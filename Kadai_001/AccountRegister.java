b3f604934cb77714def76b4fe2049f4edb95390f
kh727 <130198331+kh727@users.noreply.github.com>
2023-05-17 20:37:08 +0900

    Add files via upload

---
+++ Kadai_001/AccountRegister.java
@@ -0,0 +1,36 @@
+package login;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AccountRegister {
+	//データベース接続に使用する情報
+		final String jdbcId ="root";
+		final String jdbcPass= "password";
+		final String jdbcUrl = "jdbc:mysql://localhost:3306/test";
+	
+	public AccountRegister(AccountBeans ab) {
+		try(Connection con = DriverManager.getConnection(jdbcUrl, jdbcId, jdbcPass)){
+			
+			String sql = "INSERT INTO account (loginId, pass, name, roleId) VALUES(?, ?, ?, ?)";
+			PreparedStatement ps=con.prepareStatement(sql);
+			
+			ps.setString(1, ab.getLoginId());
+			ps.setString(2, ab.getPass());
+			ps.setString(3, ab.getName());
+			ps.setInt(4, ab.getRole());
+			
+			int r =ps.executeUpdate();
+			
+			if(r != 0) {
+				System.out.println("新規登録成功！");
+			} else {
+				System.out.println("新規登録失敗！");
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Kadai_001/logintest.java
@@ -0,0 +1,53 @@
+package login;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class logintest {
+	//データベース接続に使用する情報
+	final String jdbcId = "root";
+	final String jdbcPass = "password";
+	final String jdbcUrl = "jdbc:mysql://localhost:3306/test";
+
+	//ログインアカウントを探す
+	public AccountBeans findAccount(AccountBeans ad) {
+		
+		//戻り値の用意
+		AccountBeans returnAb = new AccountBeans();
+		
+		//データベースへ接続
+		try(Connection con = DriverManager.getConnection(jdbcUrl, jdbcId, jdbcPass)) {
+			
+			String sql ="SELECT loginId, pass, name, roleId FROM account WHERE loginId = ? AND pass = ?";
+			PreparedStatement ps= con.prepareStatement(sql);
+			
+			ps.setString(1, ad.getLoginId());
+			ps.setString(2, ad.getPass());
+			
+			ResultSet rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				//見つかったアカウント情報を戻り値にセット
+				returnAb.setLoginId(rs.getString("loginID"));
+				returnAb.setPass(rs.getString("pass"));
+				returnAb.setName(rs.getString("neme"));
+				returnAb.setRole(rs.getInt("roleId"));
+				
+				} else {
+					//アカウントがなければnullを返す
+					return null;
+					
+				}
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return returnAb;
+		
+	}
+
+}
